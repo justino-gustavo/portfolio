6ab1963a10434da78ea8dd151696c4742b0f4b07
Gustavo Justino <justinofgustavo@gmail.com>
2023-05-25 23:13:37 -0300

    fix some bugs

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,40 +1,42 @@
 <script>
-	import wifiIcon from 'bootstrap-icons/icons/wifi.svg?raw';
-	import volumeIcon from 'bootstrap-icons/icons/volume-up.svg?raw';
-	import batteryIcon from 'bootstrap-icons/icons/battery-half.svg?raw';
+	import githubIcon from 'bootstrap-icons/icons/github.svg?raw';
+	import shareIcon from 'bootstrap-icons/icons/share.svg?raw';
+	import moreIcon from 'bootstrap-icons/icons/three-dots.svg?raw';
 	import circleIcon from 'bootstrap-icons/icons/circle.svg?raw';
+
+	import MenuNotifications from './MenuNotifications.svelte';
+	import MenuOptions from './MenuOptions.svelte';
 </script>
 
 <nav>
-	<div class="left">
-		<a href=".">
+	<div>
+		<a>
 			<img src={'data:image/svg+xml,' + encodeURI(circleIcon)} alt="menu" />
 		</a>
 	</div>
-	<div class="middle">
-		<input class="show" type="checkbox" id="notifications" />
-		<label for="notifications" />
-		<div class="dropdown">
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque corrupti deleniti repudiandae soluta dolores
-				architecto omnis reprehenderit animi quos cum, iure consectetur unde quaerat odit, aperiam sit fugiat! Numquam,
-				beatae?
-			</p>
+
+	<input type="checkbox" name="menuActive" id="notifications" class="notShow" />
+	<div>
+		<label for="notifications">
+			<a> qui 25 de mai 20:42 </a>
+		</label>
+		<div class="drop center">
+			<MenuNotifications />
 		</div>
 	</div>
-	<div class="right">
-		<input class="show" type="checkbox" id="options" />
+
+	<input type="checkbox" name="menuActive" id="options" class="notShow" />
+	<div>
 		<label for="options">
-			<img src={'data:image/svg+xml,' + encodeURI(wifiIcon)} alt="wifi" />
-			<img src={'data:image/svg+xml,' + encodeURI(volumeIcon)} alt="volume" />
-			<img src={'data:image/svg+xml,' + encodeURI(batteryIcon)} alt="battery" />
+			<a>
+				<img src={'data:image/svg+xml,' + encodeURI(githubIcon)} alt="wifi" />
+				<img src={'data:image/svg+xml,' + encodeURI(shareIcon)} alt="volume" />
+				<img src={'data:image/svg+xml,' + encodeURI(moreIcon)} alt="battery" />
+			</a>
 		</label>
-		<div class="dropdown">
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque corrupti deleniti repudiandae soluta dolores
-				architecto omnis reprehenderit animi quos cum, iure consectetur unde quaerat odit, aperiam sit fugiat! Numquam,
-				beatae?
-			</p>
+
+		<div class="drop right">
+			<MenuOptions />
 		</div>
 	</div>
 </nav>
@@ -43,18 +45,7 @@
 	@bgColor: hsl(210, 55%, 5%);
 	@sectionSize: @headerSize - 2mm;
 
-	@floatMargin: 2mm;
-
-	.nav-section(@color, @radius: 9cm) {
-		border-radius: @radius;
-
-		transition: cubic-bezier(0.86, 0, 0.07, 1) 300ms;
-
-		&:hover {
-			background-color: lighten(@color, 10%);
-		}
-	}
-
+	@hoverColor: lighten(@bgColor, 10%);
 	nav {
 		box-sizing: border-box;
 		display: inline-flex;
@@ -72,39 +63,67 @@
 			height: @sectionSize;
 			min-width: @sectionSize;
 			padding: 1mm 1.5mm;
+			border-radius: 9cm;
+			transition: cubic-bezier(0.86, 0, 0.07, 1) 300ms;
 
-			.nav-section(@bgColor);
+			&:hover {
+				background-color: @hoverColor;
+			}
 
-			> label, a {
+			label {
+				display: contents;
+			}
+			a {
 				display: inline-flex;
 				align-items: center;
-				cursor: pointer;
-
 				height: 100%;
 				width: 100%;
+				color: white;
+				cursor: pointer;
+
+				&:any-link {
+					text-decoration: none;
+				}
 
 				> img {
 					margin: 0 2mm;
-
 					filter: invert();
+
+					&:first-child {
+						margin: 0 2mm 0 0;
+					}
+					&:last-child {
+						margin: 0 0 0 2mm;
+					}
+					&:only-child {
+						margin: 0;
+					}
+				}
+			}
+			.drop {
+				@spacing: 3mm;
+
+				display: none;
+
+				position: absolute;
+				top: calc(@headerSize + @spacing);
+				height: 3cm;
+				width: 3cm;
+
+				background: #000;
+				&.center {
+					left: 50%;
+					translate: -50%;
+				}
+				&.right {
+					right: @spacing;
 				}
 			}
 		}
-	}
+		> input:checked + div {
+			background-color: @hoverColor;
 
-	input.show {
-		display: none;
-
-		~ .dropdown {
-			display: none;
-
-			position: absolute;
-			top: calc(@headerSize + @floatMargin);
-			border-radius: 5mm;
-		}
-
-		&:checked {
-			~ .dropdown {
+			> .drop {
 				display: block;
 			}
 		}

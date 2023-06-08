f9f1baea8641f92675aadf2813911fdd3b1b9d1a
Gustavo Justino <justinofgustavo@gmail.com>
2023-06-07 21:36:38 -0300

    cofig to set base path

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV == 'development' ? "" : process.env.BASE_PATH
+			base: process.env.BASE_PATH || ''
 		}
 	},
 	preprocess: preprocess({
